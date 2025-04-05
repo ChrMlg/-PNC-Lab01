f029dc6f5bbf70aa83f4950b2715847471c47040
aleSaenz1429 <saenzalce1507@gmail.com>
2025-04-05 14:23:06 -0600

    Preview main

--- pnc-lab01/src/App.java
+++ pnc-lab01/src/App.java
@@ -3,12 +3,15 @@ import java.util.List;
 import Class.Player;
 import Class.Team;
 import Enum.PlayerRole;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        
-                //TODO: MAIN MENU 
+        Scanner sc = new Scanner(System.in);
+
+        int option;
+        //TODO: MAIN MENU 
         /*
         *Calcular jugadores con mas goles de un partido
             *seleccionar partido por fecha y/ o equipos usando stream
@@ -18,6 +21,45 @@ public class App {
         *calcular ganadores
         */ 
 
+        do { 
+             System.err.println("----------------------Menu Principal----------------------");
+             System.err.println("| 1)               Ver a los jugadores                    |");
+             System.err.println("| 2) Ver a los jugadores con mas de 5 goles por temporada |");
+             System.err.println("| 3)               Inserta un nuevo partido               |");
+             System.err.println("| 4)               Estadisticas por jugador               |");
+             System.err.println("| 5)                 Partidos realizados                  |");
+             System.err.println("| 6)                  Salir del sistema                   |");
+             System.err.println("|         Seleccione la opcion que desea realizar         |");
+             System.err.println("-----------------------------------------------------------");
+             
+             option = sc.nextInt();
+
+            switch (option) {
+                case 1: //Ver a los jugadores A y B
+                    
+                    break;
+                case 2: //Ver a los jugadores con mas de 5 goles en temporada
+                    break;
+
+                case 3: //Insertar un nuevo partido, aqui se saca la logica de jugadores que mas goles hicieron en el partido 
+                     break;
+
+                case 4: //Estadisticas de jugador
+                    break;
+
+                case 5: //partidos realizados, seleccionar partido por fecha/ equipo
+                    break;
+
+                case 6: //Salir del sistema
+                     System.err.println(" Gracias por preferirnos, regrese pronto! ");
+                    break;
+                default:
+                    System.err.println(" La opcion que selecciono no existe ");
+            }
+            
+        } while (option != 0);
+
+        sc.close();
 
 
     }
