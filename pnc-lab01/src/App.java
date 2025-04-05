29237d69cfd1a24bfd275d6e3d6109ebe337ec67
ChrMlg <00110219@uca.edu.sv>
2025-04-05 10:53:31 -0600

    created class for team, data classes, and interfaces for generic listing

--- pnc-lab01/src/App.java
+++ pnc-lab01/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        //TODO: MAIN MENU 
+        /*
+        *Calcular jugadores con mas goles de un partido
+            *seleccionar partido por fecha y/ o equipos usando stream
+            *seleccionar jugador con más goles
+        *seleccionar jugadores con mas de 5 goles en la temporada
+        *Insertar un partido, con parametros como goles por jugador, 
+        *calcular ganadores
+        */ 
+
+
+
     }
 }
--- pnc-lab01/src/Class/Player.java
+++ pnc-lab01/src/Class/Player.java
@@ -1,9 +1,10 @@
-public class Jugador {
+package Class;
+public class Player {
     private int id; 
     private int scoredGoals;
     private int matches;
     
-    public Jugador() {
+    public Player() {
         
     }
 
