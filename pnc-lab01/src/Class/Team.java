c152b4ceadc98bb6326238df7c1a94a38e457ebb
ChrMlg <00110219@uca.edu.sv>
2025-04-05 12:26:55 -0600

    refactored team class

--- pnc-lab01/src/Class/Team.java
+++ pnc-lab01/src/Class/Team.java
@@ -3,13 +3,47 @@ package Class;
 import java.util.LinkedList;
 
 public class Team {
+    private int id;
     private String name;
     private LinkedList<Player> players;
 
     public Team() {
     }
 
+    public Team(int id, String name, LinkedList<Player> players) {
+        this.id = id;
+        this.name = name;
+        this.players = players;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LinkedList<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(LinkedList<Player> players) {
+        this.players = players;
+    }
+
     
+
+
+
 
 
 }
